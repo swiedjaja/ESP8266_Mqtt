5759a13cc9cc5fc1d527734f14f1a29cc92e9534
Wiedjaja Atmadja <swiedjaja@gmail.com>
2021-08-30 13:11:13 +0700

    update pinout description

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,27 @@
 /*
 MQTT Example for ESP8266, how to publish and subcribe to Mqtt Topic
 
-Board: ESP12E, ESP12F, NodeMCU 86288
+Board: ESP12E, ESP12F, NodeMCU ESP8266
 Library Used:
   - beegee-tokyo/DHT sensor library for ESPx@^1.18
 	- 256dpi/MQTT@^2.5.0
+
+NodeMCU ESP8266 Pin:
+  #define D0 16
+  #define D1 5 // I2C Bus SCL (clock)
+  #define D2 4 // I2C Bus SDA (data)
+  #define D3 0
+  #define D4 2 // Same as "LED_BUILTIN", but inverted logic
+  #define D5 14 // SPI Bus SCK (clock)
+  #define D6 12 // SPI Bus MISO
+  #define D7 13 // SPI Bus MOSI
+  #define D8 15 // SPI Bus SS (CS)
+  #define D9 3 // RX0 (Serial console)
+  #define D10 1 // TX0 (Serial console)
+
+  #define S3 10
+  #define RX0 D9
+  #define TX0 D10
 */
 #include <Arduino.h>
 #include <ESP8266WiFi.h>
